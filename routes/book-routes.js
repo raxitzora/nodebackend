d5c9d77f8fbf010f75ded372794bcca2d230582a
raxitzora <raxitzora@gmail.com>
2026-04-04 23:28:35 +0530

    Book routes are created

---
+++ routes/book-routes.js
@@ -0,0 +1,10 @@
+import express from "express";
+
+const router = express.Router()
+
+router.get("/get", );
+router.get("/get/:id");
+router.post("/add",)
+router.put("/update/:id",)
+router.delete("/delete/:id")
+
