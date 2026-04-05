d6f0ee39b8d8d060eede19952829cc0bdd5fef0c
raxitzora <raxitzora@gmail.com>
2026-04-05 23:56:47 +0530

    user models completed and edited

--- models/User.js
+++ models/User.js
@@ -21,7 +21,10 @@ const userSchema = new mongoose.Schema({
     },
     role:{
         type:String,
-        enum:['user','admin']
+        enum:['user','admin'],
+        default:'user'
     }
-
 },{timestamps:true})
+
+const User = mongoose.model("User",userSchema);
+export default User;
