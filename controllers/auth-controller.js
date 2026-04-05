010695406aa7f21183c4f1cb8099f72490f61350
raxitzora <raxitzora@gmail.com>
2026-04-06 00:11:11 +0530

    auth routes are created

--- controllers/auth-controller.js
+++ controllers/auth-controller.js
@@ -1,1 +1,28 @@
 import User from "../models/User.js";
+
+//register
+const registerUser = async(req,res)=>{
+    try {
+
+        
+    } catch (error) {
+        console.log(error);
+        res.status(500).json({success:false,message:"Something went wrong"})
+        
+        
+    }
+}
+
+
+//login 
+const loginUser = async(req,res)=>{
+    try {
+        
+    } catch (error) {
+        console.log(error);
+        res.status(500).json({success:false,message:"Something went wrong"})    
+
+        
+        
+    }
+}
